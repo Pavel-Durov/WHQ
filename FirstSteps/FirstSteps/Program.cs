2cb4516e7133d25c22530209d04eeed59ce36488
Pavel Durov <cccpiscool@gmail.com>
2015-10-28 19:46:42 +0200

    init...

---
+++ FirstSteps/FirstSteps/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Diagnostics.Runtime;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FirstSteps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\Pavel\AppData\Local\Temp\Taskmgr.DMP";
+            if (File.Exists(path))
+            {
+
+
+                using (var info = DataTarget.LoadCrashDump(path))
+                {
+                    foreach (ClrInfo version in info.ClrVersions)
+                    {
+                        var runtime = version.CreateRuntime();
+                    }
+                }
+            }
+            else
+            {
+                Console.Beep();
+            }
+        }
+    }
+}
