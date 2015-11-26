c84b1b1cb51d0cd86ab3bbc62eb769e87c3dcc6d
Pavel Durov <pdurov0@gmail.com>
2015-11-26 15:42:09 +0200

    nothing special

---
+++ Assignments/Assignment_4/Model/WctApiWrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4.Model
+{
+    /// <summary>
+    /// about: https://msdn.microsoft.com/en-us/library/windows/desktop/ms681622(v=vs.85).aspx
+    /// use: https://msdn.microsoft.com/en-us/library/windows/desktop/ms681418(v=vs.85).aspx
+    /// </summary>
+    public class WctApiWrapper
+    {
+        public WctApi()
+        {
+        }
+
+       
+  
+    }
+}
