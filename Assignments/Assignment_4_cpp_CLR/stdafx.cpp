5ed00fd090ba0585167bf5ba0f3384b8252610ed
Pavel Durov <cccpiscool@gmail.com>
2015-12-23 12:26:06 +0200

    removing garbage projects

--- Assignments/Assignment_4_cpp_CLR/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// Assignment_4_cpp_CLR.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
