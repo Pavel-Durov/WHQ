80cfa943ab23f46a866d870057c64000e587420d
Pavel-Durov <pdurov0@gmail.com>
2016-02-26 12:47:09 +0200

    removing garbage

--- Assignments/Consumer/Program.cs
+++ Assignments/Consumer/Program.cs
@@ -9,18 +9,12 @@ using System.IO;
 using Assignments.Core.Handlers;
 using Microsoft.Diagnostics.Runtime;
 
-//TODO: post build -> start ../../../Assignment_3.DumpTest\bin\x86\Debug\Assignment_3.DumpTest.exe
-
 namespace Consumer
 {
     class Program
     {
-        const int PID_NOT_FOUND = 0;
-        const int ATTACH_TO_PPROCESS_TIMEOUT = 999999;
-
         static void Main(string[] args)
         {
-
             if(args != null && args.Length == 2 && !String.IsNullOrEmpty(args[0]))
             {
                 if (args[0] == "-dump" && !String.IsNullOrEmpty(args[1]))
@@ -43,21 +37,5 @@ namespace Consumer
 
             Console.ReadKey();
         }
-
-      
-        private static int GetPidFromDumpProcessTextFile()
-        {
-            int pid = PID_NOT_FOUND;
-
-            var fileContent = File.ReadAllText(@"./../../../dump_pid.txt");
-            if (!String.IsNullOrEmpty(fileContent))
-            {
-                var success = int.TryParse(fileContent, out pid);
-            }
-
-            return pid;
-        }
-
-      
     }
 }
