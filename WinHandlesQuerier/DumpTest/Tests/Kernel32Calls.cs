f1c4828e9c4f5f852b76f08917d6bc69bd1dea42
Pavel-Durov <pdurov0@gmail.com>
2016-05-11 16:24:50 +0300

    DumpTest Kernel32 Mutex calls added

--- WinHandlesQuerier/DumpTest/Tests/Kernel32Calls.cs
+++ WinHandlesQuerier/DumpTest/Tests/Kernel32Calls.cs
@@ -12,8 +12,32 @@ namespace DumpTest.Tests
     {
         public static void Run()
         {
+            MutexCalls();
             CriticalSectionCalls();
             WaitCalls();
+        }
+
+        private static void MutexCalls()
+        {
+            SECURITY_ATTRIBUTES secAttribs = new SECURITY_ATTRIBUTES();
+            secAttribs.nLength = Marshal.SizeOf(secAttribs);
+            secAttribs.bInheritHandle = 1;
+
+            var mutexAttributesPtr = Marshal.AllocCoTaskMem(Marshal.SizeOf(secAttribs));
+            var mutexName = "This-My-Mutex-Man";
+
+            Console.WriteLine($"Creating Mutex with name : '{mutexName}'");
+
+            var hMutex = CreateMutex(mutexAttributesPtr, true, mutexName);
+            var lastError = Marshal.GetLastWin32Error();
+            Console.WriteLine($"Win32 Error : {lastError}");
+
+            // It's faster to first try OpenMutex
+            IntPtr openMutex = OpenMutex((uint)(SyncObjectAccess.MUTEX_MODIFY_STATE), false, mutexName);
+
+            var lastError2 = Marshal.GetLastWin32Error();
+            Console.WriteLine($"Win32 Error : {lastError2}");
+
         }
 
         public static IntPtr section;
@@ -70,6 +94,18 @@ namespace DumpTest.Tests
             var mulRes1 = WaitForMultipleObjects(3, arr, false, 0);
             var mulRes2 = WaitForMultipleObjects(3, arr, true, int.MaxValue);
         }
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateMutex(IntPtr lpMutexAttributes, bool bInitialOwner,
+   string lpName);
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SECURITY_ATTRIBUTES
+        {
+            public int nLength;
+            public IntPtr lpSecurityDescriptor;
+            public int bInheritHandle;
+        }
 
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern void InitializeCriticalSection(out IntPtr
@@ -87,8 +123,25 @@ namespace DumpTest.Tests
         [DllImport("kernel32.dll")]
         static extern bool TryEnterCriticalSection(ref IntPtr
    lpCriticalSection);
+        /// <summary>
+        /// This value can be returned by CreateMutex() and is found in
+        /// C++ in the error.h header file.
+        /// </summary>
+        public const int ERROR_ALREADY_EXISTS = 183;
+        public const uint SYNCHRONIZE = 0x00100000;
 
-      
+        [DllImport("kernel32.dll")]
+        public static extern bool ReleaseMutex(IntPtr hMutex);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenMutex(uint dwDesiredAccess, bool bInheritHandle, string lpName);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool SetSecurityDescriptorDacl(ref SECURITY_DESCRIPTOR securityDescriptor, bool daclPresent, IntPtr dacl, bool daclDefaulted);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool InitializeSecurityDescriptor(out SECURITY_DESCRIPTOR securityDescriptor, uint dwRevision);
+
 
 
         [DllImport("kernel32.dll", SetLastError = true)]
@@ -114,5 +167,40 @@ namespace DumpTest.Tests
         public const Int32 WAIT_OBJECT_0 = 0x00;
         public const Int32 WAIT_TIMEOUT = 0x102;
         public const Int32 WAIT_FAILED = -1;
+    }
+
+    /// <summary>
+    /// Security enumeration from:
+    /// http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dllproc/base/synchronization_object_security_and_access_rights.asp
+    /// </summary>
+    [Flags]
+    public enum SyncObjectAccess : uint
+    {
+        DELETE = 0x00010000,
+        READ_CONTROL = 0x00020000,
+        WRITE_DAC = 0x00040000,
+        WRITE_OWNER = 0x00080000,
+        SYNCHRONIZE = 0x00100000,
+        EVENT_ALL_ACCESS = 0x001F0003,
+        EVENT_MODIFY_STATE = 0x00000002,
+        MUTEX_ALL_ACCESS = 0x001F0001,
+        MUTEX_MODIFY_STATE = 0x00000001,
+        SEMAPHORE_ALL_ACCESS = 0x001F0003,
+        SEMAPHORE_MODIFY_STATE = 0x00000002,
+        TIMER_ALL_ACCESS = 0x001F0003,
+        TIMER_MODIFY_STATE = 0x00000002,
+        TIMER_QUERY_STATE = 0x00000001
+    }
+
+    [StructLayoutAttribute(LayoutKind.Sequential)]
+    public struct SECURITY_DESCRIPTOR
+    {
+        public byte revision;
+        public byte size;
+        public short control;
+        public IntPtr owner;
+        public IntPtr group;
+        public IntPtr sacl;
+        public IntPtr dacl;
     }
 }
