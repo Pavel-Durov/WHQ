9ed1ead8ac622391358bc099704f5ac307d8f7da
Pavel-Durov <pdurov0@gmail.com>
2016-06-05 17:21:38 +0300

    Printing OS versions

--- WinHandlesQuerier/WaitForMultipleObjectsTest/WaitForMultipleObjectsTest.cpp
+++ WinHandlesQuerier/WaitForMultipleObjectsTest/WaitForMultipleObjectsTest.cpp
@@ -3,10 +3,24 @@
 #include<Windows.h>
 #include <iostream>
 
+#pragma warning(disable : 4996)
+
+void print_os_info()
+{
+	//http://stackoverflow.com/questions/1963992/check-windows-version
+	OSVERSIONINFOW info;
+	ZeroMemory(&info, sizeof(OSVERSIONINFOW));
+	info.dwOSVersionInfoSize = sizeof(OSVERSIONINFOW);
+
+	LPOSVERSIONINFOW lp_info = &info;
+	GetVersionEx(lp_info);
+
+	printf("Windows version: %u.%u\n", info.dwMajorVersion, info.dwMinorVersion);
+}
+
 int main()
 {
-	
-	
+	print_os_info();
 	HANDLE hEvent = CreateEvent(nullptr, TRUE, FALSE, L"Alfred");
 	HANDLE hMutex = CreateMutex(nullptr, FALSE, L"Bertha");
 	HANDLE handles[] = { hEvent, hMutex };
--- WinHandlesQuerier/WaitForSingleObjectTest/WaitForSingleObjectTest.cpp
+++ WinHandlesQuerier/WaitForSingleObjectTest/WaitForSingleObjectTest.cpp
@@ -2,15 +2,33 @@
 #include "stdafx.h"
 #include<Windows.h>
 #include <iostream>
+#include <VersionHelpers.h>
+#include <windows.h>
+#include <string>
 
+#pragma warning(disable : 4996)
+
+void print_os_info()
+{
+	//http://stackoverflow.com/questions/1963992/check-windows-version
+	OSVERSIONINFOW info;
+	ZeroMemory(&info, sizeof(OSVERSIONINFOW));
+	info.dwOSVersionInfoSize = sizeof(OSVERSIONINFOW);
+
+	LPOSVERSIONINFOW lp_info = &info;
+	GetVersionEx(lp_info);
+
+	printf("Windows version: %u.%u\n", info.dwMajorVersion, info.dwMinorVersion);
+}
 
 int main()
 {
+	print_os_info();
 	std::cout << "PID: " << GetCurrentProcessId() << std::endl;
 
 	HANDLE hEvent = CreateEvent(nullptr, TRUE, FALSE, L"Alfred");
-	
-	std::cout << "WaitForSingleObject :: handle:"<< hEvent << std::endl;
+
+	std::cout << "WaitForSingleObject :: handle:" << hEvent << std::endl;
 
 	WaitForSingleObject(hEvent, INFINITE);
 
@@ -20,4 +38,6 @@ int main()
 
 	return 0;
 }
+
+
 
