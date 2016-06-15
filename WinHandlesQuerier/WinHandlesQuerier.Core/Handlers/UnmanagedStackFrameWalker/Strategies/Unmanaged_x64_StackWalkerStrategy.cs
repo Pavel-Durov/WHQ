9cdba099e5d3633b27c676360c10188a17519006
Pavel-Durov <pdurov0@gmail.com>
2016-06-15 17:17:29 +0300

    Unmanaged_x64_StackWalkerStrategy comments

--- WinHandlesQuerier/WinHandlesQuerier.Core/Handlers/UnmanagedStackFrameWalker/Strategies/Unmanaged_x64_StackWalkerStrategy.cs
+++ WinHandlesQuerier/WinHandlesQuerier.Core/Handlers/UnmanagedStackFrameWalker/Strategies/Unmanaged_x64_StackWalkerStrategy.cs
@@ -12,11 +12,8 @@ namespace Assignments.Core.Handlers.UnmanagedStackFrame.Strategies
     /// Since it's x64 StackWalkerStrategy, 
     /// it relies on x64 Calling Convention - passing parameters to function using CPU registers. 
     /// 
-    ///https://msdn.microsoft.com/en-us/library/9z1stfyw.aspx
-    ///RCX - Volatile - First integer argument
-    ///RDX - Volatile -Second integer argument
-    ///R8 - Volatile - Third integer argument
-    ///R9 - Volatile - Fourth integer argument
+    /// x64 Software Conventions - Calling Convention : https://msdn.microsoft.com/en-us/library/zthk2dkh.aspx
+    /// x64 Software Conventions - Register Usage: https://msdn.microsoft.com/en-us/library/9z1stfyw.aspx
     /// </summary>
     internal class Unmanaged_x64_StackWalkerStrategy : UnmanagedStackWalkerStrategy
     {
@@ -27,117 +24,106 @@ namespace Assignments.Core.Handlers.UnmanagedStackFrame.Strategies
 
         Config _globalConfigs;
 
+        /// <summary>
+        /// Original Function call example: 
+        ///     void WINAPI EnterCriticalSection(LPCRITICAL_SECTION lpCriticalSection);
+        ///     
+        /// </summary>
         protected override UnifiedBlockingObject ReadCriticalSectionData(UnifiedStackFrame frame, ClrRuntime runtime)
         {
             UnifiedBlockingObject result = null;
 
             if (frame.ThreadContext != null)
             {
-                if (_globalConfigs.OsVersion == WinVersions.Win_10
-                  || _globalConfigs.OsVersion == WinVersions.Win_8
-                  || _globalConfigs.OsVersion == WinVersions.Win_8_1)
-                {
-                    //TODO: Sems like Rcx isn't moved..
-
-                    var firstParam = frame.ThreadContext.Context_amd64.Rcx;
-
-                    result = new UnifiedBlockingObject(firstParam, UnifiedBlockingType.CriticalSectionObject);
-                }
+                //1nd: CriticalSeciton pointer (HANDLE)
+                //RCX - Volatile -Second integer argument
+                //
+                //Assembly : 
+                //00007ff8`c74baa6b 488bf9          mov     rdi,rcx
+                //Rdi is Nonvolatile register
+                var criticalSectionPtr = frame.ThreadContext.Context_amd64.Rdi;
+                result = new UnifiedBlockingObject(criticalSectionPtr, UnifiedBlockingType.CriticalSectionObject);
             }
 
             return result;
         }
 
-
         /// <summary>
         /// Original Function call example: 
-        /// var mulRes2 = Functions.WaitForMultipleObjects(19, arr, true, int.MaxValue);
-        ///  
+        ///     DWORD WaitForMultipleObjects(DWORD  nCount,HANDLE* lpHandles,BOOL bWaitAll,DWORD dwMilliseconds);
         /// </summary>
         protected override void DealWithMultiple(UnifiedStackFrame frame, ClrRuntime runtime, uint pid)
         {
             if (frame.ThreadContext != null)
             {
-                if (_globalConfigs.OsVersion == WinVersions.Win_10
-                   || _globalConfigs.OsVersion == WinVersions.Win_8
-                   || _globalConfigs.OsVersion == WinVersions.Win_8_1)
+                //1st : handlesCount (DWORD)
+                //Assembly:
+                //00007ffb`bb183a98 8bd9            mov     ebx,ecx
+                //
+                //Rbx = Ebx , Rbx is a Nonvolatile register
+                var handlesCount = frame.ThreadContext.Context_amd64.Rbx;
+                if (handlesCount > Kernel32.Const.MAXIMUM_WAIT_OBJECTS)
                 {
-                    //1st : handlesCount (DWORD)
-                    var handlesCount = frame.ThreadContext.Context_amd64.Rbx;
-                    if (handlesCount > Kernel32.Const.MAXIMUM_WAIT_OBJECTS)
-                    {
-                        throw new ArgumentOutOfRangeException($"Cannot await on more then : {Kernel32.Const.MAXIMUM_WAIT_OBJECTS}, given value :{handlesCount}");
-                    }
-
-                    //2nd: Handles pointer (HANDLE)
-                    //RDX - Volatile -Second integer argument
-                    //Assembly
-                    //00007ffa`d7653a95 4c8bea          mov     r13,rdx
-                    //R13 == RDX
-                    var hArrayPtr = frame.ThreadContext.Context_amd64.R13;
-
-
-                    //3rd: WaitAll (BOOLEAN)
-                    ///R8 - Volatile - Third integer argument
-                    //
-                    //00007ffa`d7653a90 4489442444      mov     dword ptr [rsp+44h],r8d
-                    //var thirdParam = frame.ThreadContext.Context_amd64.R15;
-
-                    var rspPtr = frame.StackPointer + 44 + sizeof(ulong);
-                    byte[] buffer = new byte[sizeof(ulong)];
-                    int read = 0;
-                    bool waitAllFlagParam;
-                    if (runtime.ReadMemory(rspPtr, buffer, buffer.Length, out read))
-                    {
-                        waitAllFlagParam = BitConverter.ToBoolean(buffer, 0);
-                    }
-
-                    //4th: Timeout (DWORD) 
-                    //R9 - Volatile - Fourth integer argument
-                    //Assembly:
-                    //00007ffa`d7653a8d 458be1          mov     r12d,r9d
-                    var waitTime = frame.ThreadContext.Context_amd64.R12;
-
-                    EnrichUnifiedStackFrame(frame, runtime, pid, handlesCount, hArrayPtr);
+                    throw new ArgumentOutOfRangeException($"Cannot await for more then : {Kernel32.Const.MAXIMUM_WAIT_OBJECTS}, given value :{handlesCount}");
                 }
+
+                //2nd: Handles pointer (HANDLE)
+                //RDX - Volatile -Second integer argument
+                //Assembly
+                //00007ffa`d7653a95 4c8bea          mov     r13,rdx
+                //
+                //R13 is a Nonvolatile register
+                var hArrayPtr = frame.ThreadContext.Context_amd64.R13;
+
+
+                //3rd: WaitAll (BOOLEAN)
+                ///R8 - Volatile - Third integer argument
+                //
+                //00007ffa`d7653a90 4489442444      mov     dword ptr [rsp+44h],r8d
+                //
+                //this one fetched from the stack.
+                var rspPtr = frame.StackPointer + 44 + (ulong)IntPtr.Size;
+                byte[] buffer = new byte[IntPtr.Size];
+                int read = 0;
+                bool waitAllFlagParam;
+                if (runtime.ReadMemory(rspPtr, buffer, buffer.Length, out read))
+                {
+                    waitAllFlagParam = BitConverter.ToBoolean(buffer, 0);
+                }
+
+                //4th: Timeout (DWORD) 
+                //R9 - Volatile - Fourth integer argument
+                //Assembly:
+                //00007ffa`d7653a8d 458be1          mov     r12d,r9d
+                //
+                //R12 is a Nonvolatile register
+                var waitTime = frame.ThreadContext.Context_amd64.R12;
+
+                EnrichUnifiedStackFrame(frame, runtime, pid, handlesCount, hArrayPtr);
             }
         }
 
         /// <summary>
         ///  Original Function call example: 
-        ///    Kernel32.Functions.WaitForSingleObject(hEvent, waitTime);
-        ///    
-        /// 
+        ///    DWORD WINAPI WaitForSingleObject(HANDLE hHandle,DWORD  dwMilliseconds);
         /// </summary>
         protected override void DealWithSingle(UnifiedStackFrame frame, ClrRuntime runtime, uint pid)
         {
             if (frame.ThreadContext != null)
             {
-                if (_globalConfigs.OsVersion == WinVersions.Win_10
-                    || _globalConfigs.OsVersion == WinVersions.Win_8
-                    || _globalConfigs.OsVersion == WinVersions.Win_8_1)
-                {
-                    if (frame.ThreadContext.Is64Bit)
-                    {
+                ///RCX - Volatile - First integer argument
+                ///Handler ptr
+                ///
+                ///00007ff8`c74baa6b 488bf9          mov     rdi,rcx
+                ///
+                ///Rdi is a Nonvolatile register
+                var handle = frame.ThreadContext.Context_amd64.Rdi;
 
-                        ///RCX - Volatile - First integer argument
-                        ///Handler ptr
-                        var handle = frame.ThreadContext.Context_amd64.Rdi;
-
-                        ///RDX - Volatile -Second integer argument
-                        ///WaitAmount
-                        var watTime = frame.ThreadContext.Context_amd64.R13;
-                        //Cannot be obtained - since the value is overriden:
-                        //
-
-                        //EnrichUnifiedStackFrame(frame, handle, pid);
-
-                        //TODO:   
-                        //In manged (C#) code WaitForSingleObject function called as WaitForMultipleObjects
-                        //So it can be treated as WaitForMultipleObjects call convention - but with diffrent registers flow
-
-                    }
-                }
+                ///RDX - Volatile -Second integer argument
+                ///WaitAmount
+                ///Rsi is a Nonvolatile register
+                var watTime = frame.ThreadContext.Context_amd64.Rsi;
+                EnrichUnifiedStackFrame(frame, handle, pid);
             }
         }
     }
